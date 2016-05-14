ea2fff2e388bf549397deb96fa8ce729d353666f
Dana Shakiba <dana0550@me.com>
2016-05-14 13:33:32 -0700

    Initial Commit

---
+++ pixy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pixy
+//
+//  Created by Dana Shakiba on 5/14/16.
+//  Copyright © 2016 Appadana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pixy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pixy
+//
+//  Created by Dana Shakiba on 5/14/16.
+//  Copyright © 2016 Appadana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
